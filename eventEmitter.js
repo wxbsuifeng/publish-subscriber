387e6678c3250c4dbb5e8a337cff39617c6b47f6
wxbsuifeng <1533569724@qq.com>
2021-04-26 22:40:06 +0800

    update 实现eventEmitter.js、main.js逻辑

--- eventEmitter.js
+++ eventEmitter.js
@@ -1,17 +1,52 @@
-module.exports.eventEmitter = {
-  eventList: {},
+
+const eventList = {};
+let eventEmitter = {
   on: (event, fn) => {
-    (this.eventList[event] || (this.eventList = [])).push(fn)
-    return this.eventList;
+    (eventList[event] || (eventList[event] = [])).push(fn)
+    console.log(`订阅了${event}！`)
+    return eventEmitter;
   },
   emit: (event) => {
-    let tempArr = this.eventList[event];
-    if(!tempArr || tempArr.length === 0) {
+    let fnList = eventList[event];
+    if(!fnList || fnList.length === 0) {
       return false;
     }
 
-    tempArr.map(fn => {
-      
+    fnList.map(fn => {
+      fn();
     })
+  },
+  off: (event, fn) => {
+    let fnList = eventList[event];
+
+    if(!fnList || fnList.length === 0) {
+      return false;
+    }
+
+    if (!fn) {
+      fnList && (fnList.length = 0);
+      return eventEmitter;
+    }
+
+    for(let idx = 0; idx < fnList.length; idx++) {
+      if(idx ===  fn) {
+        fnList.splice(idx, 1);
+        break;
+      }
+    }
+    
+    console.log('取消了订阅！')
+    return eventEmitter;
+  },
+  once: (event, fn) => {
+    function onceFn (){
+      off(event, onceFn);
+      fn();
+    }
+
+    on(event, onceFn);
+    return eventEmitter;
   }
 }
+
+module.exports = eventEmitter
--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-const { eventEmitter } = require('./eventEmitter')
-// eventEmitter.on();
-// eventEmitter.emit();
+const eventEmitter = require('./eventEmitter')
+const { run } = require('./testSub')
+
+function gameStartFn() {
+  console.log('Please press alt + f4 to play games ', this.gameName);
+}
+
+class Test {
+  constructor() {
+    this.gameName = '对局1';
+  }
+
+  play() {
+    let fn = gameStartFn.bind(this)
+    eventEmitter.on('gameStart', fn);
+    run();
+    eventEmitter.off('gameStart', fn)
+  }
+}
+
+let test1 = new Test();
+test1.play();
+
